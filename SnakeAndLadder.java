8eb208a980f1ce1f81ebff4d055ec074289c0425
Tejal Gaikwad <you@example.com>
2022-09-13 23:31:43 +0530

    Added:Exact_winning_position git commit -m Added:Playing the game with 2 player

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,59 +1,32 @@
 package com.bridgelab;
 
 public class SnakeAndLadder {
-	public static final int No_play = 0;
-	public static final int Ladder = 1;
-	public static final int Snake = 2;
-	public static final int Start = 0;
-
 	public static void main(String[] args) {
-		int pos = 0, cntr1 = 0, cntr2 = 0, cntr3 = 0, cntr4 = 0;
-		System.out.println("Starting position of player is: " + Start);
-		while (pos < 100) {
-			int roll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-			cntr1++;
-			System.out.println("Number on rolled dice is : " + roll);
-			int con = (int) (Math.floor(Math.random() * 10) % 3);
-			System.out.println("Condition: " + con);
-			switch (con) {
-			case No_play:
-				System.out.println("New Position is: " + pos);
-				cntr4++;
-				break;
-			case Ladder:
-				pos = pos + roll;
-				if (pos <= 100)
-					System.out.println("New Position is: " + pos);
-				else {
-					pos = pos - roll;
-					System.out.println("Remains at same position: " + pos);
-				}
-				cntr2++;
-				break;
-			case Snake:
-				pos = pos - roll;
-				if (pos <= 100) {
-					if (pos >= 0) {
-						System.out.println("New Position is: " + pos);
-					} else {
-						pos = 0;
-						System.out.println("New Position is: " + pos);
-					}
-				} else {
-					System.out.println("New position is:" + pos);
-				}
-				cntr3++;
-				break;
-			default:
-				System.out.println("Default");
-				break;
-			}
-		}
-		System.out.println();
-		System.out.println("Player ROLLED DICE " + cntr1 + " time and got SNAKE " + cntr3 + " times and got LADDER "
-				+ cntr2 + " times and got NO PLAY " + cntr4 + " times to win the game");
-		if (pos == 100)
+		int position = 0;
+		int count = 0;
+		while (position <= 100) {
+			count++;
+			int snake = (int) Math.floor(Math.random() * 10) % 3;
+			int check = (int) Math.floor(Math.random() * 10) % 7;
+			if (snake == 1) {
+				position = position + check;
+				if (position > 100) {
+					position = position - check;
 
-			System.out.println("PLAYER WON");
+				}
+			} else if (snake == 2) {
+				position = position - check;
+				if (position < 0) {
+					position = 0;
+				}
+			} else {
+
+				count = count - 1;
+
+			}
+			System.out.println("No of times dice roll is : " + count);
+
+			System.out.println("current postion  is: " + position);
+		}
 	}
 }
