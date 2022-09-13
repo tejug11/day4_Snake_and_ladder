4da02ba34e94b5a713b9bf3e06f74100f1c7fe13
Tejal Gaikwad <you@example.com>
2022-09-13 22:28:14 +0530

    Winning position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -9,33 +9,36 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 		int pos = 0;
 		System.out.println("Starting position of player is: " + Start);
-		int roll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-		System.out.println("Number on rolled dice is : " + roll);
-		int con = (int) (Math.floor(Math.random() * 10) % 3);
-		System.out.println("Condition: " + con);
-		switch (con) {
-		case No_play:
-			System.out.println("New Position is: " + pos);
-			break;
-			
-		case Ladder:
-			pos = pos + roll;
-			System.out.println("New Position is: " + pos);
-			break;
-			
-		case Snake:
-			pos = pos - roll;
-			if (pos >= 0)
+		while (pos <= 100) {
+			int roll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+			System.out.println("Number on rolled dice is : " + roll);
+			int con = (int) (Math.floor(Math.random() * 10) % 3);
+			System.out.println("Condition: " + con);
+			switch (con) {
+			case No_play:
 				System.out.println("New Position is: " + pos);
-			else
-				pos = 0;
-			System.out.println("New Position is: " + pos);
-			break;
-			
-		default:
-			System.out.println("Default");
-			break;
+				break;
+			case Ladder:
+				pos = pos + roll;
+				System.out.println("New Position is: " + pos);
+				break;
+			case Snake:
+				pos = pos - roll;
+				if (pos >= 0)
+					System.out.println("New Position is: " + pos);
+				else
+					pos = 0;
+				System.out.println("New Position is: " + pos);
+				break;
+			default:
+				System.out.println("Default");
+				break;
+			}
 		}
+
+		if (pos >= 100)
+			System.out.println(" PLAYER WON ");
+
 	}
 
 }
